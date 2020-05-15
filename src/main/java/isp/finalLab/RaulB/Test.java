5bf64973b81632a8be6e481caefd33f595bffe4d
unknown <denisdab99@mail.ru>
2020-05-15 23:08:19 +0300

    Final Project structure

---
+++ src/main/java/isp/finalLab/RaulB/Test.java
@@ -0,0 +1,13 @@
+package isp.finalLab.RaulB;
+
+//TODO: Delete this file;
+//TODO: Add tasks
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
