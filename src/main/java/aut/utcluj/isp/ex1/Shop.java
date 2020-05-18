3dcbe22da70df7351d1b4166a0abdf04d16d6658
Banut Raul <banut.raul@yahoo.com>
2020-05-18 10:41:41 +0300

    Finished exercise 1

--- src/main/java/aut/utcluj/isp/ex1/Shop.java
+++ src/main/java/aut/utcluj/isp/ex1/Shop.java
@@ -1,5 +1,7 @@
 package aut.utcluj.isp.ex1;
 
+import java.util.Objects;
+
 /**
  * @author stefan
  */
@@ -8,20 +10,42 @@ public class Shop {
     private String city;
 
     public Shop(String name, String city) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.name = name;
+        this.city = city;
     }
 
     public Shop(String name) {
+        this.name = name;
         this.city = "";
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public String getName() {
         return name;
     }
 
-
     public String getCity() {
         return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shop shop = (Shop) o;
+        if (Objects.equals(name, shop.name) && Objects.equals(city, shop.city)) {
+            shop.toString();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop: " + name + " City: " + city;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, city);
     }
 }
